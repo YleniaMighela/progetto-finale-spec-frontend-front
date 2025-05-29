d3b63a9d9a058ac6fec0f77e60100113ff3c8f17
Ylenia <yleniamighela@gmail.com>
2025-05-29 10:30:20 +0200

    add title with icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import './Header.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSeedling } from "@fortawesome/free-solid-svg-icons";
 import Navbar from '../Navbar/Navbar'
-export default function Header() {
 
+
+export default function Header() {
 
     return (
         <header>
-            <h1>Scegli & Pianta</h1>
+            <h1 className='title'> Scegli & Pianta
+                <FontAwesomeIcon icon={faSeedling} />
+            </h1>
             <Navbar />
         </header>
     );
