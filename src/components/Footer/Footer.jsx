a2ca6a5018a05523995f7887296ef180d66aaf4e
Ylenia <yleniamighela@gmail.com>
2025-06-06 09:17:30 +0200

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,36 @@
 import './Footer.css';
 
-export default function Footer() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
+
+
+export default function Footer() {
 
     return (
         <footer>
-            <h1>piè di pagina</h1>
+
+            <div className="footer-left">
+                <span>Termini e condizioni</span>
+                <span className="footer-none">Contatti  +39 3424395784</span>
+                <span className="footer-none">email@outlook.it</span>
+                <span className="footer-none">P.IVA 56546841354864</span>
+            </div>
+            <div className="footer-right">
+                <span>Seguici anche su</span>
+                <span><FontAwesomeIcon icon={faInstagram} size="2x" color="#E1306C" /></span>
+                <span><FontAwesomeIcon icon={faFacebook} size="2x" color="#1877F2" /></span>
+                <span><FontAwesomeIcon icon={faWhatsapp} size="2x" color="#25D366" /></span>
+
+
+
+            </div>
+
+
+
+
         </footer>
 
-
-    );
+    )
 }
+
